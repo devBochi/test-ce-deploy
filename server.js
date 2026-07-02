307e7f3712b85471114e1c485ea24ef689376f39
Nahuel Montes de Oca <mdosuareznahuel@gmail.com>
2026-07-02 12:13:10 -0300

    Test api key acotadaaa

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`OK - Deploy aguante INCO - ${new Date().toISOString()}\n`);
+  res.end(`OK - INCO - ${new Date().toISOString()}\n`);
 }).listen(port, () => console.log(`Listening on ${port}`));
